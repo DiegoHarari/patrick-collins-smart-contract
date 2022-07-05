8584c1eac4baadb809eef83afca9407b597673f2
DIEGO HARARI <diegoharari@DIEGOs-MacBook-Air.local>
2022-07-05 16:07:14 +0200

    deploy changes

--- Deploy.js
+++ Deploy.js
@@ -1,29 +1,49 @@
 const ethers = require("ethers")
+
+// This package is required in order to read from the BIN and ABI files
 const fs = require("fs-extra")
-const env = require("dotenv").config()
+
+// This is what protects your PRIVATE KEY
+require("dotenv").config()
 
 async function main() {
+    //http://127.0.0.1:7545
+    // For our script to connect to the blockchain. In this case the Ganache local blockchain
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-    const encryptedJson = fs.readFileSync("./.encryptedJsonKey.json")
+
+    const encryptedJson = fs.readFileSync("./.encryptedJsonKey.json", "utf8")
     let wallet = new ethers.Wallet.fromEncryptedJsonSync(
         encryptedJson,
         process.env.PRIVATE_KEY_PASSWORD
     )
-    console.log(wallet)
     wallet = await wallet.connect(provider)
+
     const abi = fs.readFileSync(
         "./SimpleStorage_sol_SimpleStorage.abi",
-        "uctf8"
+        "utf-8"
     )
     const binary = fs.readFileSync(
         "./SimpleStorage_sol_SimpleStorage.bin",
-        "utf8"
+        "utf-8"
     )
+
+    // To deploy the contract
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
-    const contract = await contractFactory.deploy()
+    console.log("Deploying Contract")
+    const contract = await contractFactory.deploy({ gasLimit: 6721975 })
     await contract.deployTransaction.wait(1)
-    const currentFavNumber = await contract.retrieve()
+
+    console.log(`Contract Address: ${contract.address}`)
+
+    // ## Retrieve Button .. Just like in Remix .. to see data ##
+    const currentFavoriteNumber = await contract.retrieve()
+    console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`)
+
+    // ## 'Update Number' ... Just like in Remix ... to update the data ##
     const transactionResponse = await contract.store("7")
+    const transactionReceipt = await transactionResponse.wait(1)
+    const updatedFavoriteNumber = await contract.retrieve()
+    console.log(`Updated Favorite Number is: ${updatedFavoriteNumber}`)
 }
 
 main()
